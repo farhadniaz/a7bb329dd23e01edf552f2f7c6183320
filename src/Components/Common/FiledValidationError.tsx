645139ab24dbb6504a30b5d55598b39a38160588
Farhad Azarbarzinniaz <farhad00niaz@gmail.com>
2021-03-01 02:40:46 +0330

    add react-hook-form and validation error component

---
+++ src/Components/Common/FiledValidationError.tsx
@@ -0,0 +1,28 @@
+import React, { FC } from "react";
+import { FieldError } from "react-hook-form";
+import styled from 'styled-components';
+const Wrapper = styled.div`
+color:red;
+`;
+interface IProps {
+  message?: string;
+  error: (FieldError | undefined)[] | undefined | FieldError;
+}
+const FiledValidationError: FC<IProps> = props => {
+  let { message, error } = props;
+  let defaultMessage;
+  // @ts-ignore
+  switch (error?.type) {
+    case "required":
+      defaultMessage = "This filed is required!";
+      break;
+    default:
+      defaultMessage = "";
+      break;
+  }
+  const e: FieldError | undefined = error && (error instanceof Array ? error[0] : error);
+
+  return error ? <Wrapper>{e?.message || message || defaultMessage}</Wrapper> : null;
+};
+
+export default FiledValidationError;
